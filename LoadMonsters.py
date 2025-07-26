fa3343e736d72bc354180ae421849ef92e845e88
Sam Ellinger <sambeep1234@gmail.com>
2025-07-25 19:46:38 -0500

    Updated to use objects/classes

--- LoadMonsters.py
+++ LoadMonsters.py
@@ -20,26 +20,76 @@
 
 import random
 import json
+from typing import List
 
-def run_a_fight(monsters:list):
+class Attack:
+    def __init__(self,action: str,dice: str,to_hit: int, attack_name: str):
+        self.action = action
+        self.dice = dice
+        self.to_hit = to_hit
+        self.attack_name = attack_name
+
+    def compute_damage(self):
+        dice=self.dice
+        return roll_the_dice(dice)
+    
+    def does_attack_hit(self,AC:int):
+        "attacks hit or miss (rolling a d20)"
+        to_hit=self.to_hit
+        x=random.randint(1,20)
+        if x==1: #fumble
+            return False
+        if x==20: #crit
+            return True
+        if x+to_hit>=AC:
+            return True
+        else:
+            return False
+
+
+class Monster:
+    def __init__(self,data:dict):
+        self.name = data["Name"]
+        self.initiative = 0
+        self.hit_dice = data["Hit Dice"]
+        self.hp = 0
+        self.actions = data["Actions"]
+        self.ac = data["AC"]
+        print(f"create new monster named {self.name}")
+        
+    def get_attacks(self)->List[Attack]:
+        "gets attacks for the monsters"
+        result=[]
+        has_multiattack= self.actions["Multiattack"]
+        for action in self.actions:
+            if action=="Multiattack":
+                continue
+            dice=(self.actions[action]["base damage"])
+            to_hit=(self.actions[action]["to hit"])
+            attack =Attack(action,dice,to_hit, action)
+            result.append(attack)
+        return result
+
+
+def run_a_fight(monsters:List[Monster]):
     "runs a fight between the first two monsters in the list until one is dead."
 
     m1=monsters[0]
     m2=monsters[1]
-    print(m1["Name"], "vs.", m2["Name"])
+    print(m1.name, "vs.", m2.name)
 
     fight_order=roll_for_initiative(m1,m2)
     i=1
     for m in fight_order:
-        print(m["Name"], "goes", order_text(i))
+        print(m.name, "goes", order_text(i))
         i=i+1
 
-    m1["HP"]=roll_the_dice(m1["Hit Dice"])
-    print(m1["Name"], "has", m1["HP"], "HP.")
-    m2["HP"]=roll_the_dice(m2["Hit Dice"])
-    print(m2["Name"], "has", m2["HP"], "HP.")
+    m1.hp=roll_the_dice(m1.hit_dice)
+    print(m1.name, "has", m1.hp, "HP.")
+    m2.hp=roll_the_dice(m2.hit_dice)
+    print(m2.name, "has", m2.hp, "HP.")
     Round_number=1
-    while m1["HP"]>0 and m2["HP"]>0:
+    while m1.hp>0 and m2.hp>0:
         print("=== Round",Round_number)
         run_a_round(fight_order)
         Round_number=Round_number+1
@@ -47,11 +97,11 @@ def run_a_fight(monsters:list):
 
     print()
 
-    if m1["HP"]>0:
-        print(m1["Name"], "wins with", m1["HP"], "HP left!")
-    if m2["HP"]>0:
-        print(m2["Name"], "wins with", m2["HP"], "HP left!")
-    if m1["HP"]<=0 and m2["HP"]<=0:
+    if m1.hp>0:
+        print(m1.name, "wins with", m1.hp, "HP left!")
+    if m2.hp>0:
+        print(m2.name, "wins with", m2.hp, "HP left!")
+    if m1.hp<=0 and m2.hp<=0:
         print("Both die.")
     print()
 
@@ -59,45 +109,19 @@ def run_a_round(fight_order:list):
     "fight a single round"
     for m_active in fight_order:
         m_opponent=find_opponent(fight_order, m_active)
-        print(m_active["Name"], "attacks", m_opponent["Name"])
-        attacks= get_attacks(m_active)
+        print(m_active.name, "attacks", m_opponent.name)
+        attacks= m_active.get_attacks()
         for a in attacks:
-            print(m_active["Name"], "uses", a["attack name"])
-            if does_attack_hit(a,m_opponent["AC"]):
+            print(m_active.name, "uses", a.attack_name)
+            if a.does_attack_hit(m_opponent.ac):
 
-                dmg=compute_damage(a)
-                m_opponent["HP"]=m_opponent["HP"]-dmg
-                print(m_opponent["Name"], "takes", dmg, "damage.", m_opponent["Name"], "has", m_opponent["HP"], "HP left.")
-                if m_opponent["HP"]<=0:
+                dmg=a.compute_damage()
+                m_opponent.hp=m_opponent.hp-dmg
+                print(m_opponent.name, "takes", dmg, "damage.", m_opponent.name, "has", m_opponent.hp, "HP left.")
+                if m_opponent.hp<=0:
                     return
             else:
-                print(m_active["Name"], "misses!")
-
-def does_attack_hit(attack:dict,AC:int):
-    "attacks hit or miss (rolling a d20)"
-    to_hit=attack["to hit"]
-    x=random.randint(1,20)
-    if x==1: #fumble
-        return False
-    if x==20: #crit
-        return True
-    if x+to_hit>=AC:
-        return True
-    else:
-        return False
-
-def get_attacks(monster:dict):
-    "gets attacks for the monsters"
-    result=[]
-    has_multiattack= monster["Actions"]["Multiattack"]
-    for action in monster["Actions"]:
-        if action=="Multiattack":
-            continue
-        dice=(monster["Actions"][action]["base damage"])
-        to_hit=(monster["Actions"][action]["to hit"])
-        attack ={"attack name":action, "dice":dice, "to hit":to_hit}
-        result.append(attack)
-    return result
+                print(m_active.name, "misses!")
 
 def roll_the_dice(dice:str):
     #To do: make it read any kind of dice combination
@@ -110,12 +134,6 @@ def roll_the_dice(dice:str):
         totaldmg=totaldmg+random.randint(1,int(dmgdice))
     return totaldmg
     
-def compute_damage(attack:dict):
-    dice=attack["dice"]
-    return roll_the_dice(dice)
-
-    raise Exception("No attacks.")
-
 def find_opponent(monsters: list,current_monster:dict):
     for m in monsters:
         if m ==current_monster: continue
@@ -125,12 +143,12 @@ def find_opponent(monsters: list,current_monster:dict):
 def roll_for_initiative(monster_1:dict,monster_2:dict):    
     "calculates who goes first"
     while True:
-        monster_1["initiative"]=random.randint(1,20)
-        monster_2["initiative"]=random.randint(1,20)
+        monster_1.initiative=random.randint(1,20)
+        monster_2.initiative=random.randint(1,20)
 
-        if monster_1["initiative"]>monster_2["initiative"]:
+        if monster_1.initiative>monster_2.initiative:
             return [monster_1,monster_2]
-        elif monster_1["initiative"]<monster_2["initiative"]:
+        elif monster_1.initiative<monster_2.initiative:
             return [monster_2,monster_1]
 
 def order_text(n:int)->str:
@@ -145,6 +163,8 @@ f=open("MonsterStats.json")
 text=f.read()
 monsters=json.loads(text)
 #print(monsters)
+monsters = [Monster(m) for m in monsters]
+#print(monsters[2].name)
 
 run_a_fight(monsters)
 
