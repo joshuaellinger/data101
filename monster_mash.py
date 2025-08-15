d5180826b0a2c59f04b39df1ea23a26d69e5b0b1
Sam Ellinger <sambeep1234@gmail.com>
2025-08-15 11:22:11 -0500

    ui

---
+++ monster_mash.py
@@ -0,0 +1,13 @@
+from screen_welcome import screen_welcome
+from screen_monsterselect import screen_monsterselect
+from screen_fight import screen_fight
+from screen_result import screen_result
+
+def main():
+    screen_welcome()
+    screen_monsterselect()
+    screen_fight()
+    screen_result()
+
+if __name__=="__main__":
+    main()
